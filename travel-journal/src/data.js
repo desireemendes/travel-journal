e947ce78b9985dfea3faca7bc2fdec48158b8251
Desiree Mendes <desireefrost@live.ca>
2022-11-27 18:42:01 -0500

    add data for journal

---
+++ travel-journal/src/data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        title: "Cruise",
+        location: "Honduras",
+        googleMapsUrl: "https://goo.gl/maps/1odr82gpHHza7tYW9",
+        startDate: "21 Dec, 2021",
+        endDate: "24 Dec, 2021",
+        description: "Roatan is known for its barrier reef and was home to 5000 pirates at one point",
+        imageUrl: "https://images.unsplash.com/photo-1625106497878-0dee231cd4e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cm9hdGFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+    }
+]
