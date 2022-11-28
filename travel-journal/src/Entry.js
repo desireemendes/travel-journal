6ea4a62d0d98ef782536bd4e43a81badc16d7b24
Desiree Mendes <desireefrost@live.ca>
2022-11-27 23:48:48 -0500

    link to google map location

--- travel-journal/src/Entry.js
+++ travel-journal/src/Entry.js
@@ -3,9 +3,13 @@ import './Entry.css'
 function Entry (props) {
 return (
     <section className="journal-entry">
-        <p>{props.googleMapsUrl}</p>
-        <p>{props.location}</p>
-        <img src={props.imageUrl} alt="" className="travel-photo" />
+        <div className="location">
+        <a className="location-url" href={props.googleMapsUrl}>
+          View on Google Maps
+        </a>
+        <p id="title">{props.location}</p>
+        </div>
+        <img className="travel-photo" src={props.imageUrl} alt={props.title} />
         <span>{props.startDate} - {props.endDate}</span>
         <p>{props.title}</p>
         <p>{props.description}</p>
