ad4052ccdad48e8cc4816150a0b8f0803931b3fe
Desiree Mendes <desireefrost@live.ca>
2022-11-28 00:44:45 -0500

    install font awesome

--- travel-journal/src/Entry.js
+++ travel-journal/src/Entry.js
@@ -3,13 +3,15 @@ import './Entry.css'
 function Entry (props) {
 return (
     <section className="journal-entry">
+        <div className="img-box">
+        <img className="travel-photo" src={props.imageUrl} alt={props.title} />
+        </div>
         <div className="location">
         <a className="location-url" href={props.googleMapsUrl}>
           View on Google Maps
         </a>
         <p id="title">{props.location}</p>
         </div>
-        <img className="travel-photo" src={props.imageUrl} alt={props.title} />
         <span>{props.startDate} - {props.endDate}</span>
         <p>{props.title}</p>
         <p>{props.description}</p>
