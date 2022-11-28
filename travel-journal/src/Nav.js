74dad90045ffda8868ad66e9c67286635b0021e2
Desiree Mendes <desireefrost@live.ca>
2022-11-28 01:12:29 -0500

    additional nav styling

--- travel-journal/src/Nav.js
+++ travel-journal/src/Nav.js
@@ -3,7 +3,7 @@ import navlogo from "./images/icons8-earth-globe-96.png"
 function Nav () {
     return (
         <nav>
-<h1>my travel journal</h1>
+<h1 className='nav-title'>my travel journal</h1>
 <img src={navlogo} alt="globe" className="nav-logo"/>
 <a target="_blank" href="https://icons8.com/icon/123622/earth-globe"></a>
         </nav>
