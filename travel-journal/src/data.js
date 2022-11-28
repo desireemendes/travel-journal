4abe0efa61fbc0d875d85dc65d1714fe5a8ac4ad
Desiree Mendes <desireefrost@live.ca>
2022-11-27 23:51:34 -0500

    add more journal entries

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -7,5 +7,27 @@ export default [
         endDate: "24 Dec, 2021",
         description: "Roatan is known for its barrier reef and was home to 5000 pirates at one point",
         imageUrl: "https://images.unsplash.com/photo-1625106497878-0dee231cd4e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cm9hdGFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-    }
+    },
+    {
+        key: 2,
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/681n4jdijgdt3uGS6",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description:
+          "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3lkbmV5JTIwb3BlcmElMjBob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+      },
+      {
+        key: 3,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/5J6KAPeSgM8CHQwB7",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description:
+          "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://images.unsplash.com/photo-1510071652223-530cba0e0948?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bm9yd2F5JTIwZmpvcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+      },
 ]
